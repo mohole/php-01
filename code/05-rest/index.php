cb1b178d6f469a76d9389693853cfc9efd147bde
Salvatore Laisa <moebiusmania@gmail.com>
2021-01-26 17:01:11 +0100

    refinements, POST email still not working

--- code/05-rest/index.php
+++ code/05-rest/index.php
@@ -1,5 +1,21 @@
-<?php
+<?php 
 
+$condition = isset($_GET["dateonly"]);
 
+$today = date('d/m/Y');
+$time = date('H:i');
+
+if( $condition ) {
+    $response = array(
+        'date' => $today,
+    );
+} else {
+    $response = array(
+        'date' => $today,
+        'time' => $time
+    );
+}
+
+echo(json_encode($response));
 
 ?>
--- code/06-email/index.php
+++ code/06-email/index.php
@@ -1,5 +1,35 @@
 <?php 
 
+/* 
+POST /email
+{
+    to: 'test@email.com',
+    subject: 'Test subject',
+    message: '.....'
+} 
+Response
+{
+    status: 'OK/FAIL',
+}
+*/
 
+$to = $_POST["to"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+$mail = mail($to, $subject, $message);
+
+$request = array(
+    'to' => $to,
+    'subject' => $subject,
+    'message' => $message
+);
+
+$response = array(
+    'status' => $mail,
+    'request' => $request,
+);
+
+echo(json_encode($response));
 
 ?>
